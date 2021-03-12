e157fc3ad4bf09d6fa18d5d640d9631296354ee8
RaizenV2 <andrei.damian.0409@gmail.com>
2021-03-12 10:17:46 -0500

    tema2

--- tema2/hw2.py
+++ tema2/hw2.py
@@ -2,14 +2,13 @@ import numpy as np
 import random
 from scipy import linalg
 import math
-import copy
 
 
 def check_symmetric(a, rtol=1e-05, atol=1e-08):
     return np.allclose(a, a.T, rtol=rtol, atol=atol)
 
 
-def verify_matrix(M):
+def verify_matrix(M):  # de implementat epsilon
 
     if not check_symmetric(M):
         print("The matrix is not simmetric")
@@ -75,108 +74,234 @@ def generate_matrix(n, min, max):
 
 
 def generate_simmetric_matrix(n, min, max):
-    simmetric_matrix = np.random.randint(min, max, size=(n, n))
-    simmetric_matrix = (simmetric_matrix + simmetric_matrix.T)/2
+    while True:
+        simmetric_matrix = np.random.randint(min, max, size=(n, n))
+        simmetric_matrix = (simmetric_matrix + simmetric_matrix.T)/2
+        if verify_matrix(simmetric_matrix):
+            break
+
     return np.array(simmetric_matrix)
 
 
-def cholesky(A):
+def lppFormula2(p, d, A, eps):
+    s = 0
+    for j in range(0, p):
+        s = s + (A[p][j] * A[p][j])
+
+    if (math.sqrt(d[p] - s)) <= eps:
+        return eps
+    else:
+        return math.sqrt(d[p] - s)
+
+
+def lipFormula2(i, p, A, eps):
+    if i > p:  # partea inferioara a lui A
+        Ai = p
+        Ap = i
+    else:  # partea superioara a lui A
+        Ai = i
+        Ap = p
+
+    s = 0
+    for j in range(0, p):
+        s = s + (A[i][j] * A[p][j])
+
+    if ((A[Ai][Ap] - s) / A[p][p]) <= eps:
+        return eps
+    else:
+        return (A[Ai][Ap] - s) / A[p][p]
+
+
+def cholesky3(A, d, eps):
     n = len(A)
-    L = [[0.0] * n for i in range(n)]
-    for i in range(n):
-        for k in range(i + 1):
-            tmp_sum = sum(L[i][j] * L[k][j] for j in range(k))
-            if i == k:
-                L[i][k] = round(math.sqrt(A[i][i] - tmp_sum), 2)
+    ret = A
+
+    for j in range(n):  # n pasi (coloanele lui L)
+        # print("j=",j)
+        for i in range(j, n):  # calculam elementele de sub diagonala principala
+            # print("  i= ", i)
+            if i == j:
+                ret[i][j] = lppFormula2(i, d, ret, eps)
             else:
-                L[i][k] = round(1.0 / L[k][k] * (A[i][k] - tmp_sum), 2)
-    return np.array(L)
+                ret[i][j] = lipFormula2(i, j, ret, eps)
+        # print("\n")
+
+    return np.array(ret)
 
 
-def DS(M, b):
-    n = len(M)
-    x = [0]*n
-    x[0] = b[0]/M[0][0]
+def determinant(A):
+    detA = 1
+    for i in range(len(A)):
+        detA = detA * A[i][i]
+    return (detA)
 
+
+def matrixConfig(x, d):
+    n = len(x)
+    matrixResulted = [[0.0] * n for i in range(n)]
+    for i in range(n):
+        for j in range(i+1, n):
+            matrixResulted[i][j] = x[i][j]
+    return np.array(matrixResulted)
+
+
+def calul_epsilon():
+    u = 1
+    while 1+u > 1:
+        u /= 10
+    return u * 10
+
+
+def calcul_xChol(A, b, eps):
+    n = len(A)
+    solutie = [eps for i in range(n)]
+
+    # notam y = L_trans * x
+    # rezolvam L * y = b
+    y = [eps]*n
+    y[0] = b[0]/A[0][0]
     for i in range(1, n):
         sum = 0
         for j in range(i):
-            sum += M[i][j]*x[j]
-        x[i] = sum/M[i][i]
-    
-    print(f"DS x: {x}\n ")
+            sum = sum + A[i][j] * y[j]
+        y[i] = (b[i] - sum) / A[i][i]
 
-    return x
+    # rezolvam L_trans * x = y
+    x = [eps]*n
+    x[n-1] = y[n-1] / A[n-1][n-1]
 
-
-def IS(M, b):
-    n = len(M)
-    x = [0]*n
-    x[n-1] = b[n-1] / M[n-1][n-1]
-
-    for i in range(n-1, -1, -1):
+    for i in range(n-2, -1, -1):
         sum = 0
-        for j in range(i, n):
-            sum += M[i][j]*x[j]
-        x[i] = sum/M[i][i]
+        for j in range(i+1, n):
+            sum = sum + A[j][i] * x[j]
+            if sum == eps:
+                sum = sum * (-1)
+        x[i] = (y[i] - sum) / A[i][i]
 
-    print(f"IS x: {x}\n ")
-
-    return x
+    return np.array(x)
 
 
-def determinant_cholesky(A, n):
-    detA = 1
+def get_init_matrix(A, eps, d):
+    n = len(A)
+    rez = [[eps] * n for i in range(n)]
     for i in range(n):
-        detA *= A[i][i]*A[i][i]
-    return (detA)
+        for j in range(n):
+            if i == j:
+                rez[i][j] = d[i]
+            else:
+                if i > j:
+                    rez[i][j] = A[j][i]
+                else:
+                    rez[i][j] = A[i][j]
+
+    return np.array(rez)
+
+
+def calcul_inversa(A, eps):
+    n = len(A)
+    rezultat = [[0.0] * n for i in range(n)]
+
+    for k in range(n):
+        # rezolv A * x = l_k
+        # adica L * L_trans * x = l_k
+        # adica L * y = l_k
+        l = [eps for t in range(n)]  # generez l_k
+        l[k] = 1
+
+        # rezolv L * y = l_k
+        y = [eps]*n
+        y[0] = l[0]/A[0][0]
+        for i in range(1, n):
+            sum = 0
+            for j in range(i):
+                sum = sum + A[i][j] * y[j]
+            y[i] = (l[i] - sum) / A[i][i]
+
+        # rezolv L_trans * x = y
+        x = [eps]*n
+        x[n-1] = y[n-1] / A[n-1][n-1]
+        for i in range(n-2, -1, -1):
+            sum = 0
+            for j in range(i+1, n):
+                sum = sum + A[j][i] * x[j]
+                if sum == eps:
+                    sum = sum * (-1)
+            x[i] = (y[i] - sum) / A[i][i]
+
+        # actualizez coloana k
+        for i in range(n):
+            rezultat[i][k] = x[i]
+
+    return np.array(rezultat)
 
 
 if __name__ == "__main__":
 
-    x = get_matrix_ordered("matricea_ideala.txt")
+    eps = calul_epsilon()
+    ok = True
+    while ok:
+        option = int(
+            input("\n\n\n\n1. Tastatura \n2. Fisier \n3. Random\nType your option:"))
+        if option == 1:
+            x = get_matrix_input()
+            d = [0.0 for i in range(len(x))]
+            for i in range(0, len(x)):
+                d[i] = x[i][i]
+            ok = False
+        else:
+            if option == 2:
+                x = get_matrix_ordered("matricea_ideala.txt")
+                d = [0.0 for i in range(len(x))]
+                for i in range(0, len(x)):
+                    d[i] = x[i][i]
+                ok = False
+            else:
+                if option == 3:
+                    x = generate_simmetric_matrix(4, 1, 50)
+                    d = [0.0 for i in range(len(x))]
+                    for i in range(0, len(x)):
+                        d[i] = x[i][i]
+                    ok = False
+                else:
+                    print("Not a valid option!\n\n\n")
+                    ok = True
+
     if verify_matrix(x):
         print("The matrix is positive definite and simmetric\n")
+        A = matrixConfig(x, d)
     else:
         print("The matrix is NOT positive definite and simmetric")
         exit()
 
-    x_copy = copy.deepcopy(x)
-    cholesky_matrix = cholesky(x)
-
-    b = generate_b_for_matrix(len(x), 10, 100)
-
-    print(f"B generated {b}\n")
+    A = cholesky3(A, d, eps)
 
     print(f"The chosen one is: \n {x}\n")
 
-    print(f"Cholesky Matrix: {cholesky_matrix}\n")
+    print(f"\nA:\n {A}\n")
 
-    print(
-        f"Determinant calculand Cholesky:\n{determinant_cholesky(x, len(x))}\n")
+    print(f"Determinantul matricii A:\n{determinant(A)}\n")
 
-    initial_matrix = np.dot(cholesky_matrix, cholesky_matrix.T)
+    b = generate_b_for_matrix(len(x), 10, 100)
+    # b = [12, 6, 13, 20]
+    print(f"B generated {b}\n")
 
-    print(f"The initial matrix should be:\n {initial_matrix}\n")
+    xChol = calcul_xChol(A, b, eps)
+    print(f"\nxChol :", xChol)
 
-    initial_matrix = np.round(initial_matrix)
+    euclidianNorm = np.linalg.norm(
+        np.dot(get_init_matrix(A, eps, d), xChol) - b, 2)
+    print(f"Norma euclidiana este: {euclidianNorm}\n")
 
-    print(f"The initial matrix should be:\n {initial_matrix}\n")
-
-    Chol_v1 = DS(initial_matrix, b)
-
-    Chol_v2 = IS(initial_matrix, b)
-
-    rez1 = np.linalg.norm(np.dot(initial_matrix, Chol_v1) - b, 2)
-
-    rez2 = np.linalg.norm(np.dot(initial_matrix, Chol_v2) - b, 2)
-
-    print(f"rez1: {rez1} \n rez2: {rez2} \n")
-
-    p, L, U = linalg.lu(initial_matrix)
-    solutiee = np.linalg.solve(initial_matrix, b)
+    p, L, U = linalg.lu(get_init_matrix(A, eps, d))
+    solutiee = np.linalg.solve(get_init_matrix(A, eps, d), b)
 
     print(f"L: {L}\n")
     print(f"U :{U}\n")
     print(solutiee)
-    # print(generate_matrix(10, 10, 100))
+
+    aproxInversa = calcul_inversa(A, eps)
+    print(f"\nAproxiarea inversei matricei A este:\n {aproxInversa}\n")
+    Abibl = linalg.inv(get_init_matrix(A, eps, d))
+    print(f"\nA_bibl este:\n {Abibl}\n")
+    normaMatrice = np.linalg.norm(aproxInversa - Abibl)
+    print(f"\nNorma este:\n {normaMatrice}\n")
